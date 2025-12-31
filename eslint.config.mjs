bbddab7d88c31e423ba03eae04e7e92876251198
Shadae <shadaejadeellmers@gmail.com>
2025-12-31 20:48:57 +1300

    fix eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,31 +1,38 @@
 import { defineConfig, globalIgnores } from 'eslint/config'
 import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
+import { default as parser } from '@typescript-eslint/parser'
+import nextPlugin from '@next/eslint-plugin-next'
 
-export default defineConfig(
-  [
-    globalIgnores([
-      'node_modules/**',
-      '.next/**',
-      'out/**',
-      'build/**',
-      'next-env.d.ts',
-      'src/generated/**',
-      '**/*.js',
-    ]),
-  ],
-  tseslint.config({
+export default defineConfig([
+  globalIgnores([
+    'node_modules/**',
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+    'src/generated/**',
+    '**/*.js',
+    'eslint.config.mjs',
+  ]),
+  eslint.configs.recommended,
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  {
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      eslint.configs.recommended,
-      ...tseslint.configs.recommended,
-      ...tseslint.configs.strictTypeChecked,
-    ],
     languageOptions: {
+      parser,
       parserOptions: {
+        project: 'tsconfig.json',
         tsconfigRootDir: new URL('.', import.meta.url).pathname,
-        project: './tsconfig.json',
       },
     },
-  })
-)
+    plugins: {
+      '@next/next': nextPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      '@typescript-eslint/no-misused-promises': 'off',
+    },
+  },
+])
