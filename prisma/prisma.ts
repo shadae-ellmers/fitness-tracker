438752b27fd44f5acffb2c8c4af1d164c606c445
Shadae <shadaejadeellmers@gmail.com>
2025-12-31 20:41:27 +1300

    resolve prisma generated type errors

--- prisma/prisma.ts
+++ prisma/prisma.ts
@@ -1,18 +1,9 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '../src/generated/prisma'
 
 declare global {
   var prisma: PrismaClient | undefined
 }
 
-let prisma: PrismaClient
+export const prisma = global.prisma ?? new PrismaClient()
 
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient()
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient()
-  }
-  prisma = global.prisma
-}
-
-export default prisma
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma
